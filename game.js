4b470b467ca0f0e19239354e40ee6b95b644993a
Didac Bigorda <didacbigorda@gmail.com>
2019-01-20 17:50:08 +0100

    More or less functioning scrolling perspective

--- game.js
+++ game.js
@@ -3,60 +3,131 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
-var width = 640;
-var height = 480;
+var width = 1280;
+var height = 720;
 
 //Finds the center of the canvas
-var centerWidth = width / 2;
-var centerHeight = height / 2;
-var razon = 1.25;
-var zDepth = centerHeight;
+var centerX = width / 2;
+var centerY = height / 2;
+var razon = 1.26;
+var zDepth = centerY;
 var colorChange = false;
 var green1 = '#006400';
 var green2 = '#009900';
 
 function _clearCanvas() {
-  context.clearRect(0, 0, width, height);
+  context.fillStyle = '#009900';
+  context.fillRect(0, 0, width, height);
 }
 
+//context.beginPath();
+//context.moveTo(width/6,0);
+//context.lineTo(width/4,centerY)
+//context.stroke();
+
+
+// Alternating colors. Rendering line by line
+
+//for (var y = height ; y > 0 ; y--) {
+//  if (y == parseInt(zDepth)) {
+//    if (colorChange == false) {
+//      context.strokeStyle = green2;
+//    } else {
+//      context.strokeStyle = green1;
+//    };
+//    zDepth /= razon;
+//    colorChange = !colorChange; 
+//  }
+//  var posY = centerY + y;
+//
+//  context.beginPath();
+//  context.moveTo(0, posY);
+//  context.lineTo(width, posY);
+//  context.stroke();
+//}
+//
 function _updateReason() {
+
   if (razon > 1.2) {
-    razon -= 0.01;
+    razon -= 0.0009;
   } else {
-    razon = 1.25;
+    razon = 1.26;
   }
-}
-
-
-function loop() {
-
-
-
 
 }
+
+var frames = 60;
+var timerId = 0;
+
+timerId = setInterval(gameLoop, 1000/frames);
+
+var 
+
 function gameLoop() {
 
   _updateReason();
   _clearCanvas();
   for (var y = height ; y > 1 ; y /= razon) {
-    //_clearCanvas();
-    var posY = centerHeight + y;
-    //debugger;
-    context.beginPath();
-    context.moveTo(0, posY);
-    context.lineTo(width, posY);
-    context.stroke();
-    //context.fillRect(0, posY, width, posY);
+
+    var posY = centerY + y;
+    //context.strokeStyle = 'white';
+    //context.lineWidth = 5;
+//
+    //context.beginPath();
+    //context.moveTo(width - width, height - 40);  
+    //context.lineTo(width/2 - 40, centerY + 2)
+    //context.stroke();
+//
+    //context.beginPath();
+    //context.moveTo(width, height - 40);
+    //context.lineTo(width - width/2 + 40, centerY + 2);
+    //context.stroke();
+
+    context.lineWidth = 2;
+    context.strokeStyle = green1;
+
+    if (colorChange == true) {
+      context.fillStyle = green2;
+      colorChange = !colorChange;
+    } else if (colorChange == false) {
+      context.fillStyle = green1;
+      colorChange = !colorChange;
+    }
+
+    context.fillRect(0, posY, width, y/2);
+debugger;
+
+    //context.beginPath();
+    //context.moveTo(0, posY);
+    //context.lineTo(width, posY);
+    //context.stroke();
+
+    context.fillStyle = 'blue';
+    context.fillRect(width - width, height - height, width, centerY + 10);
+
+    //context.globalAlpha = 0.1;
+
+
+    //I need to save previous posY to know the height of the rectangle
+
+    //if (colorChange == false) {
+    //  context.fillStyle = green2;
+    //  context.fillRect(0, posY, width, 2);
+    //  colorChange = true;
+    //} else if (colorChange == true) {
+    //  context.fillStyle = green1;
+    //  context.fillRect(0, posY, width, 2);
+    //  colorChange = false;
+    //}
     
   }
-  
-  window.requestAnimationFrame(gameLoop);
+ // window.requestAnimationFrame(gameLoop);
 
 }
 
-window.requestAnimationFrame(gameLoop);
+//window.requestAnimationFrame(gameLoop);
 
-
+//
 //  _updateReason();
 //  _clearCanvas();
 //  for (var y = height ; y > 1 ; y /= razon) {
@@ -76,9 +147,6 @@ window.requestAnimationFrame(gameLoop);
 //
 //
 //function gameLoop() {
-  
-
-  
 //  }
 //  window.requestAnimationFrame(gameLoop);
 //
